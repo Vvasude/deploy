9d0759fb5d78e0457e37da4aed9ffef4e4227d8c
Vvasude <varnesh.vasudevan@gmail.com>
2024-07-26 12:25:00 -0400

    fixed for build

--- app/(routes)/Accountants/Nimal-Vinayagamoorthy/page.tsx
+++ app/(routes)/Accountants/Nimal-Vinayagamoorthy/page.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Members } from '@/app/components/MemberInfo/Members';  // Adjust path as needed
 import ContactCard from '@/app/components/MemberPage/ContactCard';
 import LogoAndContactInfo from '@/app/components/MemberPage/LogoAndContactInfo';
-// import ServicesSection from '@/app/components/MemberPage/ServicesSection';
+import ServicesSection from '@/app/components/MemberPage/ServicesSection';
 import AboutUsAndMapSection from '@/app/components/MemberPage/AboutUsAndMapSection';
 import { IoCheckmarkCircle } from "react-icons/io5";  // Import the necessary icons
 
-const { Firstname, Lastname, logo, address, phone, email, website, iframe, aboutus, sectionItem1, sectionItem2, sectionItem3 } = member;
-<ServicesSection services={services} />
+
+
 const MemberPage: React.FC = () => {
     const memberId = '34'; // Manually set the ID here for each page
 
@@ -17,7 +17,7 @@ const MemberPage: React.FC = () => {
         return <div>Member not found</div>;
     }
 
-    const { Firstname, Lastname, logo, address, phone, email, website, iframe, aboutus } = member;
+    const { Firstname, Lastname, logo, address, phone, email, website, iframe, aboutus, sectionItem1, sectionItem2, sectionItem3 } = member;
     // Create the services array based on available section items
     const services = [
         { icon: IoCheckmarkCircle, description: sectionItem1 },
@@ -37,7 +37,7 @@ const MemberPage: React.FC = () => {
                 website={website}
             />
             <AboutUsAndMapSection aboutus={aboutus} iframe={iframe} />
-            {/* <ServicesSection /> */}
+            <ServicesSection services={services} />
             <div className="w-full max-w-8xl bg-white rounded-lg shadow-xl">
                 <ContactCard />
             </div>
